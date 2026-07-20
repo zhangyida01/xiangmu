919a82973fa5009f7485611ecd3c27788476aaab
Administrator <admin@example.com>
2026-07-20 15:07:36 +0800

    fix: remove BOM from ProjectAccountController

--- src/main/java/com/delivery/management/controller/ProjectAccountController.java
+++ src/main/java/com/delivery/management/controller/ProjectAccountController.java
@@ -1,4 +1,4 @@
-﻿package com.delivery.management.controller;
+package com.delivery.management.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
