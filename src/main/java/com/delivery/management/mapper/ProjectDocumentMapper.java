e1da6b45f2fda582e3f4d81137195e5674ee94bd
Administrator <admin@example.com>
2026-07-21 09:38:32 +0800

    feat: add ProjectDocumentMapper

---
+++ src/main/java/com/delivery/management/mapper/ProjectDocumentMapper.java
@@ -0,0 +1,9 @@
+package com.delivery.management.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.delivery.management.entity.ProjectDocument;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectDocumentMapper extends BaseMapper<ProjectDocument> {
+}
